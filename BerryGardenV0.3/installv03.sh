c9fc6dd8f07704b52f15707ef3ce8ae995e88b54
Fussel-fun <justdo420@proton.me>
2026-06-05 11:03:50 +0200

    Update Option include

--- BerryGardenV0.3/installv03.sh
+++ BerryGardenV0.3/installv03.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo updating .bashrc
 echo "echo Starting BerryGarden" >> .bashrc
-echo "berryg03" >> .bashrc
+echo "sudo berryg03" >> .bashrc
 echo Updating System 
 sudo apt update && sudo apt upgrade 
 sudo apt-get update && sudo apt-get upgrade
@@ -20,7 +20,6 @@ echo Running Gcc
 cd /
 sudo git clone https://github.com/Fussel-fun/BerryGarden
 cd /BerryGarden/BerryGardenV0.3
-chmod u+x updatev03.sh
 gcc BerryGardenMon.c -o monitor -l wiringPi
 gcc BerryGardenmain.c -o berryg03 -l wiringPi
 echo copie berryg03 and monitor
@@ -28,10 +27,12 @@ sudo cp berryg03 /usr/bin
 sudo cp log.md /usr/bin
 sudo cp monitor /usr/bin
 sudo cp updatev03.sh /usr/bin
+cd /
+sudo chmod u+x updatev03.sh
 echo remove berryg03.elf
+cd /BerryGarden/BerryGardenV0.3
 rm berryg03
 echo Add Auto Start on Login
-cd /BerryGarden/BerryGardenV0.3
 echo Open Readme...
 nano READMEV0.3.md
 
