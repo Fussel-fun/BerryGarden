2006b6c28defb8237d356482d53e12c77eddff6b
Fussel-fun <justdo420@proton.me>
2026-06-07 18:08:14 +0200

    installerpy

--- BerryGardenV0.4/BerryGardenmain.c
+++ BerryGardenV0.4/BerryGardenmain.c
@@ -48,10 +48,10 @@ int main()
         // print Options while no Option is Choosen 
         do
         {
-            printf("Choose Option:\n\nOptions: \n1 = LIGHT MONITOR\n2 = SOIL MONITOR\n3 = LOGS\n4 = Run Monitor\n5 = UPDATE\n6 = GPIOOFF\n7 = Temp\n8 = TMUX\n9 = Turn Off GPIO\n");
+            printf("Choose Option:\n\nOptions: \n1 = LIGHT MONITOR\n2 = SOIL MONITOR\n3 = LOGS\n4 = Run Monitor\n5 = UPDATE\n6 = GPIOOFF\n7 = Temp\n8 = TMUX\n9 = SSH Quit\n");
             scanf("%i", &in);
         }
-        while (in == 0);
+        while (in != QUIT);
     // Open Menu Points 
     mainmenu(in);    
     }
